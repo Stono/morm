1d0eedca39181331c0f3b5f00ea57d47b6b0eba9
Karl Stoney <karl@jambr.co.uk>
2014-09-13 09:49:05 +0100

    Included sqlite as a dependency, for test purposes.  Reenabled the data access tests

--- test/dal.test.js
+++ test/dal.test.js
@@ -1,21 +1,15 @@
 'use strict';
-var Dal = require('../lib/dal');
+var Dal = require('./stubs').SqlLiteDal;
 var should = require('should');
 
-describe.skip('Data Access', function() {
-  var config = {
-    user: 'example_user',
-    password: 'example_user_password',
-    server: '172.19.104.11',
-    database: 'example_database'
-  };
+describe('Data Access', function() {
   it('Should initialise ok', function() {
-    var dal = new Dal(config);
+    var dal = new Dal();
     should(dal).not.eql(null);
   });
 
   it('Should throw an error if an execute fails', function(done) {
-    var dal = new Dal(config);
+    var dal = new Dal();
     dal.execute('SELECT * FROM some_table')
       .then(null, function(err) {
         should(err).not.eql(null);
@@ -24,7 +18,7 @@ describe.skip('Data Access', function() {
   }); 
 
   it('Should not throw an error if the command is OK', function(done) {
-    var dal = new Dal(config);
+    var dal = new Dal();
     dal.execute('SELECT * FROM example_table')
       .then(function(rs) {
         should(rs).not.eql(null);
---
+++ test/stubs.js
@@ -0,0 +1,36 @@
+'use strict';
+var when = require('when');
+var sqlite3 = require('sqlite3').verbose();
+
+var SqlLiteDal = function() {
+  var db = new sqlite3.Database(':memory:');
+
+  (function() {
+    db.serialize(function() {
+      db.run('CREATE TABLE example_table (column1 TEXT, column2 TEXT)');
+    });
+  })();
+
+  var execute = function(sql) {
+    return when.promise(function(resolve, reject) {
+      db.serialize(function() {
+        db.all(sql, function(err, rows) {
+          if(err) {
+            reject(err);
+            return;
+          } else {
+            resolve(rows);
+          }
+        }); 
+      });
+    });
+  };
+
+  return {
+    execute: execute
+  };
+};
+
+module.exports = {
+  SqlLiteDal: SqlLiteDal
+};
